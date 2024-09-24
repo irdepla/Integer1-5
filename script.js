6e96f9b64fab411b4413c724e3b87a0f40ffc058
irdepla <rixsiboyevislombek@gmail.com>
2024-09-25 04:20:17 +0500

    Integer & Begin

---
+++ script.js
@@ -0,0 +1,21 @@
+// // Integer 1
+
+// let l = prompt("Enter value for L: ")
+// let result = parseInt(l / 100)
+// alert( l + " sm da " + result + "m bor.")
+
+
+
+
+// Integer 2
+
+// let m = prompt("Enter value for M: ")
+// let result = parseInt(m / 1000)
+// alert( m + " kg da " + result + "tonna bor.")
+
+
+// Integer 3
+
+let m = prompt("Enter value for M: ")
+let result = parseInt(m / 1024)
+alert( m + " kg da " + result + "tonna bor.")
