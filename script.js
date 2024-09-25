377457e53fcf2c3114692594d38e728b6371d80e
irdepla <rixsiboyevislombek@gmail.com>
2024-09-25 05:29:47 +0500

    Integer & Begin

--- script.js
+++ script.js
@@ -16,6 +16,87 @@
 
 // Integer 3
 
-let m = prompt("Enter value for M: ")
-let result = parseInt(m / 1024)
-alert( m + " kg da " + result + "tonna bor.")
+// let m = prompt("Enter value for M: ")
+// let result = parseInt(m / 1024)
+// alert( m + " bayt " + result + " Kb ga teng.")
+
+
+
+// Integer 4
+
+
+// let a = prompt("Enter value for a: ")
+// let b = prompt("Enter value for b: ")
+// let result = parseInt(a / b)
+// alert( "A kesmada B kesmani " + result + " marta joylashtirish mumkin.")
+
+
+// Integer 5
+
+// let a = prompt("Enter value for a: ")
+// let b = prompt("Enter value for b: ")
+// let result = parseInt(a % b)
+// alert(result)
+
+
+
+
+
+// Begin 6
+
+
+// let a = prompt("Enter value for a: ")
+// let b = prompt("Enter value for b: ")
+// let c = prompt("Enter value for c: ")
+// let v = a * b * c
+// let s = 2 * (a*b + b*c + a*c)
+// alert("Paralelipedning hajmi " + v + " ga teng.")
+// alert("Paralelipedning to'la sirti " + s + " ga teng.")
+
+
+
+
+// Begin 7
+
+
+// let r = prompt("Enter value for r: ")
+// let l = 2 * Math.PI * r
+// let s = Math.PI * r ** 2
+// alert("Doiraning uzunligi " + l + " ga teng.")
+// alert("Doiraning yuzasi " + s + " ga teng.")
+
+
+// Begin 8
+
+
+// let a = parseFloat(prompt("Enter value for a: "))
+// let b = parseFloat(prompt("Enter value for b: "))
+// let sum = (a + b) / 2
+// alert(sum)
+
+
+// Begin 9
+
+
+
+// let a = parseFloat(prompt("Enter value for a: "))
+// let b = parseFloat(prompt("Enter value for b: "))
+// let sum = Math.sqrt(a * b)
+// alert(sum)
+
+
+// Begin 10
+
+
+let a = parseFloat(prompt("Enter value for a: "))
+let b = parseFloat(prompt("Enter value for b: "))
+let sum = a + b
+alert("Yig'indi: " + sum)
+
+let sum1 = a * b
+alert("Ko'paytma: " + sum1)
+
+let pow1 = a ** 2
+alert("Kvadrati: " + pow1)
+let pow2 = b ** 2
+alert("Kvadrati: " + pow2)
